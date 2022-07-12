d984dbd42bf46bf7a3e607adb1f0b869e071c2d3
Aaron Hubber <aaronhubber@gmail.com>
2022-07-12 12:58:09 +0100

    more pdf fucntions

--- frontend/src/components/FavoriteWorkshop.js
+++ frontend/src/components/FavoriteWorkshop.js
@@ -1,18 +1,40 @@
 import React from "react"
 import FavWorkshops from "./FavWorkshops"
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
 
 const FavouriteWorkshops = ({favoriteWorkshop}) => {
 
     const bestWorkshop = favoriteWorkshop?.map((workshop) => {
         return <FavWorkshops workshop = {workshop}/>
     })
+
+    const printRef = React.useRef();
+
+    const handleDownloadPdf = async () => {
+    const element = printRef.current;
+    const canvas = await html2canvas(element);
+    const data = canvas.toDataURL('image/png');
+
+    const pdf = new jsPDF();
+    const imgProperties = pdf.getImageProperties(data);
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+        const pdfHeight =
+        (imgProperties.height * pdfWidth) / imgProperties.width;
+
+    pdf.addImage(data, 'PNG', 0, 0, pdfWidth, pdfHeight);
+    pdf.save('WORKSHOP.pdf');
+    };
+
     return (
         <>
         <div>
         <h3>Favourite Workshops:</h3>
-        {bestWorkshop}
+        <div ref={printRef}>{bestWorkshop}</div>
         <div id="map"></div>
-        
+        <button type="button" onClick={handleDownloadPdf}>
+        Download as PDF
+        </button>
         </div>
         </>
     )
--- frontend/src/components/Feeling.js
+++ frontend/src/components/Feeling.js
@@ -1,38 +1,15 @@
 import React from "react"
-import html2canvas from 'html2canvas';
-import { jsPDF } from 'jspdf';
+
 
 
 const Feeling = ({feeling}) => {
 
 let byteSource = "data:image/png;base64," + feeling.picture
-    const printRef = React.useRef();
-  
-    const handleDownloadPdf = async () => {
-      const element = printRef.current;
-      const canvas = await html2canvas(element);
-      const data = canvas.toDataURL('image/png');
-  
-      const pdf = new jsPDF();
-      const imgProperties = pdf.getImageProperties(data);
-      const pdfWidth = pdf.internal.pageSize.getWidth();
-      const pdfHeight =
-        (imgProperties.height * pdfWidth) / imgProperties.width;
-  
-      pdf.addImage(data, 'PNG', 0, 0, pdfWidth, pdfHeight);
-      pdf.save('FEELING.pdf');
-    };
+
 
     return (
         <>
-        <div>
-        <button type="button" onClick={handleDownloadPdf}>
-        Download as PDF
-        </button>
-        <div ref={printRef}>{feeling.title}<img src = {byteSource} value={feeling}></img></div>
-        </div>
-  
-        
+        <div>{feeling.title}<img src = {byteSource} value={feeling}></img></div>
         </>
     )
 }
