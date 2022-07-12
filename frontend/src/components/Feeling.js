720bade26518fd4fd49318c73abe2349fca8f338
Aaron Hubber <aaronhubber@gmail.com>
2022-07-12 11:44:58 +0100

    pdf working

--- frontend/src/components/Feeling.js
+++ frontend/src/components/Feeling.js
@@ -1,14 +1,42 @@
 import React from "react"
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
 
 
 const Feeling = ({feeling}) => {
 
+let byteSource = "data:image/png;base64," + feeling.picture
+    const printRef = React.useRef();
+  
+    const handleDownloadPdf = async () => {
+      const element = printRef.current;
+      const canvas = await html2canvas(element);
+      const data = canvas.toDataURL('image/png');
+  
+      const pdf = new jsPDF();
+      const imgProperties = pdf.getImageProperties(data);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight =
+        (imgProperties.height * pdfWidth) / imgProperties.width;
+  
+      pdf.addImage(data, 'PNG', 0, 0, pdfWidth, pdfHeight);
+      pdf.save('FEELING.pdf');
+    };
+
     return (
         <>
-        <h4>I am feeling...</h4>
-        <p className="fac-workshops">{feeling.title}</p>
+        <div>
+        <button type="button" onClick={handleDownloadPdf}>
+        Download as PDF
+        </button>
+        <div ref={printRef}>{feeling.title}<img src = {byteSource} value={feeling}></img></div>
+        </div>
+  
+        
         </>
     )
 }
+
+  
 
 export default Feeling
