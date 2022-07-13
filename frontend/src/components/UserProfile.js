8f7daaf4cdeac8c0197ea33a8249f2ba128f1d53
Aaron Hubber <aaronhubber@gmail.com>
2022-07-13 09:09:32 +0100

    removed button

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import { Routes, Route} from 'react-router-dom'
 import styled from 'styled-components'
 import MyProfileContainer from '../containers/MyProfileContainer';
 
@@ -31,23 +31,17 @@ text-align:center;
 
 const UserProfile = ({profile , selectProfile}) => {
 
-  const navigate = useNavigate();
-
 let byteSource = "data:" + profile.contentType + ";base64," + profile.profilepicture.picture
 
 const handleProfileSelect = () => {
   selectProfile(profile);
 };
 
-const handleMyProfile = () => {
-  navigate('/myprofile')
-}
 
   return (
     <ProfileTile>
      <ProfilePic width = "200" src={byteSource} onClick = {handleProfileSelect}/>
      <ProfileName>{profile.name}</ProfileName>
-     <button type="button" onClick = {handleMyProfile}>Full Profile</button>
      <Routes>
           <Route path="/myprofile" element={<MyProfileContainer />} />
       </Routes>
